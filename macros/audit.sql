57120f2548e44c60f2a62cd8db08ac995051ff2d
remisharoon <remisharoon@gmail.com>
2026-02-06 23:11:52 +0400

    Changes  region_summary.sql

--- macros/audit.sql
+++ macros/audit.sql
@@ -1,19 +1,23 @@
 {% macro log_run_start() %}
-    {% set create_sql %}
-        create table if not exists {{ target.database }}.{{ target.schema }}.audit_run (
-            run_started_at timestamp_ntz,
-            invocation_id string,
-            target_name string
-        )
-    {% endset %}
+    {% if target.name == 'ci' %}
+        {% do log('Skipping audit_run hook for ci target.', info=true) %}
+    {% else %}
+        {% set create_sql %}
+            create table if not exists {{ target.database }}.{{ target.schema }}.audit_run (
+                run_started_at timestamp_ntz,
+                invocation_id string,
+                target_name string
+            )
+        {% endset %}
 
-    {% do run_query(create_sql) %}
+        {% do run_query(create_sql) %}
 
-    {% set insert_sql %}
-        insert into {{ target.database }}.{{ target.schema }}.audit_run
-        (run_started_at, invocation_id, target_name)
-        values (current_timestamp(), '{{ invocation_id }}', '{{ target.name }}')
-    {% endset %}
+        {% set insert_sql %}
+            insert into {{ target.database }}.{{ target.schema }}.audit_run
+            (run_started_at, invocation_id, target_name)
+            values (current_timestamp(), '{{ invocation_id }}', '{{ target.name }}')
+        {% endset %}
 
-    {% do run_query(insert_sql) %}
+        {% do run_query(insert_sql) %}
+    {% endif %}
 {% endmacro %}
--- models/marts/finance/region_summary.sql
+++ models/marts/finance/region_summary.sql
@@ -1,8 +1,5 @@
 {{ config(materialized='table') }}
 
-{% set region_list = var('regions', []) %}
-{% if region_list | length == 0 %}
-    {% set region_list = dbt_utils.get_column_values(ref('dim_customers'), 'customer_region') %}
-{% endif %}
+{% set region_list = var('regions', ['North America', 'EMEA', 'APAC', 'LATAM', 'Unknown']) %}
 
 {{ generate_region_summary_sql(region_list) }}
